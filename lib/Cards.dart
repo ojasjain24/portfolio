099cae5b3e11e0ba4d9aeedefac4944a7a248df6
ojas <59116312+ojasjain24@users.noreply.github.com>
2022-03-31 16:56:13 +0530

    Works Section

---
+++ lib/Cards.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:neon_widgets/neon_widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'models/iconLinkModel.dart';
+
+Color descriptionColor = Colors.lime;
+Color nameColor = Colors.deepOrangeAccent;
+Color containerSpreadColor = Colors.green.withOpacity(0.5);
+
+Widget buildsCard(
+    {required String assetImage,
+    required String name,
+    required String description,
+    required List<iconLinkModel>? links}) {
+  return oNeonContainer(
+    clipBehaviour: Clip.antiAlias,
+    lightSpreadRadius: 5,
+    lightBlurRadius: 15,
+    borderWidth: 3,
+    borderRadius: BorderRadius.circular(10),
+    containerColor: Colors.black,
+    spreadColor: containerSpreadColor,
+    width: 300,
+    padding: const EdgeInsets.only(
+      left: 10,
+      right: 10,
+      bottom: 10,
+    ),
+    child: Column(
+      children: [
+        Container(
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Image.asset(
+              assetImage,
+              width: 200,
+              height: 200,
+              fit: BoxFit.fitWidth,
+            )),
+        Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                oNeonText(
+                  blurRadius: 10,
+                  isSoftWrap: true,
+                  textAlign: TextAlign.center,
+                  text: name,
+                  spreadColor: nameColor,
+                  textSize: 25,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                oNeonText(
+                  textAlign: TextAlign.justify,
+                  text: description,
+                  spreadColor: descriptionColor,
+                  textSize: 15,
+                ),
+                Wrap(
+                    alignment: WrapAlignment.start,
+                    crossAxisAlignment: WrapCrossAlignment.start,
+                    children: [
+                      ...(links != null
+                          ? links.map((e) => InkWell(
+                                onTap: () {
+                                  launch(e.link);
+                                },
+                                child: Image.asset(
+                                  e.imageAddress,
+                                  width: 40,
+                                  height: 40,
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              ))
+                          : []),
+                    ])
+              ],
+            )),
+      ],
+    ),
+  );
+}
--- lib/dataFile.dart
+++ lib/dataFile.dart
@@ -1,4 +1,5 @@
-import 'package:portfolio/iconLinkModel.dart';
+import 'package:portfolio/models/BuildsModel.dart';
+import 'package:portfolio/models/iconLinkModel.dart';
 
 class portfolioDetails {
   static String myName = "Ojas Jain";
@@ -26,5 +27,46 @@ class portfolioDetails {
         "assets/images/dart.png", "https://pub.dev/packages/neon_widgets"),
     iconLinkModel("assets/images/instagram.png",
         "https://www.instagram.com/casual_photography___/"),
+  ];
+
+  static List<BuildsModel> experienceList = [
+    BuildsModel(
+        assetImage: "assets/images/EnigmaRunner.png",
+        name: "Enigma Runner",
+        description:
+            "Enigma Runner is a platformer game, in which the player has to collect artifacts at the end of every level to complete the story. There are some mind boggling twists at the end of every level, which the player needs to overcome to clear the level. Currently It has 4 levels and can be played on IOS, Android and windows",
+        links: [
+          iconLinkModel("assets/images/google-play.png",
+              "https://play.google.com/store/apps/details?id=com.Affix.EnigmaRunner")
+        ]),
+    BuildsModel(
+        assetImage: "assets/images/512.png",
+        name: "Affix Chat",
+        description:
+            "Affix is an end to end encrypted chat application for android devices. Apart from chat and group chat, Affix also Provides video calling, audio calling, file transfer, screen share, public and private group options, etc. Affix also provides multiple themes so that users can customize the app according to their taste.",
+        links: [
+          iconLinkModel("assets/images/google-play.png",
+              "https://play.google.com/store/apps/details?id=com.affixchat.chatappv0")
+        ]),
+    BuildsModel(
+        assetImage: "assets/images/neon_widgets_logo.png",
+        name: "Neon Widgets",
+        description:
+            "Neon_widgets is a cool open source flutter library which provides Neon versions of most commonly used widgets. These widgets are fully customizable, which can be used to give neon touch to other widgets as well. This library is compatible with all the platforms. This portfolio also uses this library",
+        links: [
+          iconLinkModel("assets/images/dart.png",
+              "https://pub.dev/packages/neon_widgets"),
+          iconLinkModel("assets/images/github.png",
+              "https://github.com/ojasjain24/neon_widgets_flutter"),
+        ]),
+    BuildsModel(
+        assetImage: "assets/images/qrlogo.png",
+        name: "Quick Scanner",
+        description:
+            "Quick scanner is a QR code and Barcode scanner app for android devices which is very compact in terms of size. It saves scanned history in local storage which can be viewed and deleted in the app itself.",
+        links: [
+          iconLinkModel("assets/images/google-play.png",
+              "https://play.google.com/store/apps/details?id=com.Affix.qrcodescanner")
+        ]),
   ];
 }
