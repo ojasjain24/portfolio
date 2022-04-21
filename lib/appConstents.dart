12ab9bfac95ce624bbc4da712ce6f495bc5c0d82
ojas <59116312+ojasjain24@users.noreply.github.com>
2022-04-21 10:37:30 +0530

    fixes

--- lib/appConstents.dart
+++ lib/appConstents.dart
@@ -12,3 +12,20 @@ double CardTitleFontSize = 25;
 double CardRadius = 10;
 Color MainSpreadColor =
     Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+
+const MaterialColor primaryBlack = MaterialColor(
+  _blackPrimaryValue,
+  <int, Color>{
+    50: Color(0xFF000000),
+    100: Color(0xFF000000),
+    200: Color(0xFF000000),
+    300: Color(0xFF000000),
+    400: Color(0xFF000000),
+    500: Color(_blackPrimaryValue),
+    600: Color(0xFF000000),
+    700: Color(0xFF000000),
+    800: Color(0xFF000000),
+    900: Color(0xFF000000),
+  },
+);
+const int _blackPrimaryValue = 0xFF000000;
